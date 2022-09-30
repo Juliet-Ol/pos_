0a6cf4d8ba2249b93dbb6379e75ae97358f065c2
Juliet <agutu@gmail.com>
2022-09-30 17:18:47 +0300

    update customer search functionality

--- customer.py
+++ customer.py
@@ -16,14 +16,15 @@ def customer_menu():
     
     while True:
         print("""use this short codes: 
-        press 1 - to create a new customer, 
-        press 2 - delete a customer,
-        press 3 - search a customer, 
-        press 4 - update customer details
+        press 1 - Create a new customer.
+        press 2 - Search a customer.
+        press 3 - Delete a customer. 
+        press 4 - Update customer details.
         press 5 - go back to main menu""") 
 
         short_code = input()
 
+    #Create a customer
         if short_code == '1':
             print("create a new customer")
 
@@ -69,38 +70,39 @@ def customer_menu():
             print(f"new customer  {first_name} {last_name} {mobile_number} has been created")
             print('\n')
 
-            
+    #Search for a customer     
 
         elif short_code == '2':
-            print("search customer to delete")   
-            
-            def search_name():
-                letter = input("enter letter: ").upper()
-                IniFile = open ("customer.txt","r")
-                for s in IniFile:
-                    if s[0] == letter:
-                    
-                        print(s)
-            search_name() 
-                         
-            # search_customer = input()
-            if Customer(first_name, mobile_number):
-                    
-                    # search_customer = p1(search_customer)
-                    search_customer = Customer(first_name, last_name, mobile_number) 
-                    print(f"{search_customer.first_name} {search_customer.last_name}")
-                    print('-' * 20)
+            print("search for a customer")  
+            print("""use this short codes: 
+                press 1 - Search by first name.
+                press 2 - Search by second name.
+                press 3 - Phone number. 
+                press 4 - Search by customer id.
+                press 5 - go back to main menu""") 
 
-                    print(f"Mobile number.......{search_customer.mobile_number}")
-                    print(f"First name.......{search_customer.first_name}")
-            else:
-                    print("That customer does not exist")
+            short_code = input()
 
-            with open("customer.txt", "r") as file_input:
-                with open("newfile.txt", "w") as output: 
-                    for line in file_input:
-                        if line.strip("\n") != "nickname_to_delete":
-                            output.write(line)           
+            search_by = input("Search:....").upper()
+            with open("customer.txt","r") as fp:
+                for line in fp:
+                    name = line.upper()
+                    line_list = name.split( )
+                    if line_list[1] == search_by:
+                        print(line)
+
+                    elif line_list[2] == search_by:
+                        print(line) 
+
+                    elif line_list[3] == search_by: 
+                        print(line)
+
+                    elif line_list[0] == search_by + ".":
+                        print(line) 
+
+    #Delete a customer 
+               
+                 
 
 
         elif short_code == '5':
