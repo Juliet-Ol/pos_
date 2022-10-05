971782129a958fcff3ad3fd734671e699f1de1f6
Juliet <agutu@gmail.com>
2022-10-05 21:52:24 +0300

    modify search menu for products

--- product.py
+++ product.py
@@ -1,14 +1,14 @@
 class Product:    
     
-    def __init__(self, product_name, product_price):
+    def __init__(self, product_name, product_price, product_quantity):
 
-        # self.product_id = product_Id
+        # self.product_id = product_id
         self.product_name = product_name
         self.product_price = product_price
-        # self.quantity = quantity    
+        self.product_quantity = product_quantity    
 
     def __str__(self):
-        return f"{self.product_name} {self.product_price} \n"         
+        return f"{self.product_name} {self.product_price} {self.product_quantity} \n"         
 
  
 
@@ -18,8 +18,8 @@ def product_menu():
     while True:
         print("""use this short codes: 
         press 1 - add a product, 
-        press 2 - delete a product,
-        press 3 - search a product, 
+        press 2 - search a product,
+        press 3 - delete a product, 
         press 4 - update product
         press 5 - go back to main menu""") 
 
@@ -29,21 +29,58 @@ def product_menu():
         if short_code == '1':
             print("create a new product") 
 
-        print("product name.....")  
-        product_name = input()
+            print("product name.....")  
+            product_name = input()
 
-        print("product price....")
-        product_price = input()
-        
-        p1 = Product(product_name, product_price)
-        
-        print (p1)
-        text_file.write(f"{p1.product_name} {p1.product_price}\n")
-        text_file.close()  
+            print("product price....")
+            product_price = input()
 
-        print('\n')
-        print(f"new product {product_name}  {product_price} has been created")
-        print('\n')
+            print("product quantity....")
+            product_quantity = input()
+            
+            p1 = Product(product_name, product_price, product_quantity)
+            
+            print (p1)
+            text_file.write(f"{p1.product_name} {p1.product_price} {p1.product_quantity}\n")
+            text_file.close()  
+
+            print('\n')
+            print(f"new product {product_name} {product_price} {product_quantity} has been created")
+            print('\n')
+
+
+    #Search for a product    
+
+        elif short_code == '2':
+            print("search for a product")  
+            print("""use this short codes: 
+                press 1 - Search product by name.
+                press 2 - price.
+                press 3 - product id. 
+                press 5 - print all products""") 
+
+            short_code = input()
+
+            search_by = input("Search:....").upper()
+            with open("product.txt","r") as fp:
+                for line in fp:
+                    item = line.upper()
+                    line_list = item.split( )
+                    if line_list[0] == search_by:
+                        print(line)
+
+                    elif line_list[1] == search_by:
+                        print(line)
+
+            # Print all the data on the database
+            with open("product.txt", "r") as f:
+                print (f.read())               
+                         
+
+        elif short_code == '5':
+            from pos import main_menu
+        
+            main_menu()
 
 if __name__ == "__main__":
 
